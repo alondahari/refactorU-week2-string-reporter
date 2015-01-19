159e64a9ff3b127c898c0ef3fc5438dbe345395d
Alon Dahari <climbinghobo@gmail.com>
2015-01-19 14:41:57 -0700

    add third character, lower and upper case

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 var word = prompt('Enter anything:');
 console.log('verbatim: ' + word);
 console.log('Length: ' + word.length);
+console.log('Third Character: ' + (word.charAt(2) || "String too short"));
+console.log('lower case: ' + word.toLowerCase());
+console.log('lower case: ' + word.toUpperCase());

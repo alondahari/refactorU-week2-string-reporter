d372797b5724a0918a34fe36e88f116c1a0eb067
Alon Dahari <climbinghobo@gmail.com>
2015-01-19 14:46:17 -0700

    add substring

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ console.log('Length: ' + word.length);
 console.log('Third Character: ' + (word.charAt(2) || "String too short"));
 console.log('lower case: ' + word.toLowerCase());
 console.log('lower case: ' + word.toUpperCase());
+console.log(word + ' is a pretty lame word');
+console.log('Substring: ' + ((word.length > 3) ? word.substring(1,4) : "String too short"));
